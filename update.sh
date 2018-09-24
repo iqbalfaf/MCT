5d484f3b45523077bd4eb3265722f37d14fb7176
Moch Iqbal <iqbalfaf127@gmail.com>
2018-09-24 13:37:40 -0400

    Update MCT

--- update.sh
+++ update.sh
@@ -13,11 +13,13 @@ sleep 1
 echo " Curl Installed " >> $log 2>$1
 sleep 1
 echo " Downloading Script... " >> $log 2>&1
-wget -O $PREFIX/bin/mct https://raw.githubusercontent.com/iqbalfaf/MCT/master/mct.sh
+curl https://raw.githubusercontent.com/iqbalfaf/MCT/master/mct.sh >> mct
 sleep 1
 echo " Script Downloaded " $log 2>&1
 sleep 1
 echo " Setting Up Script... " $log 2>&1
+rm $PREFIX/bin/mct
+mv mct $PREFIX/bin
 chmod +x $PREFIX/bin/mct
 sleep 1
 echo " Script Setting Up" $log 2>&1
