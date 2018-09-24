805e622ffa2a0eeb5d0bb2ebd81dcd637bfd0fe3
Moch Iqbal <iqbalfaf127@gmail.com>
2018-09-24 13:35:31 -0400

    Update MCT

--- update.sh
+++ update.sh
@@ -2,6 +2,16 @@
 log=$HOME/mct-update.log
 echo " Updating Script... " >> $log 2>&1
 sleep 1
+echo " Update Repo... " >> $log 2>$1
+apt update
+sleep 1
+echo " Update Selesai " >> $log 2>$1
+sleep 1
+echo " Installing Curl " >> $log 2>$1
+apt install curl
+sleep 1
+echo " Curl Installed " >> $log 2>$1
+sleep 1
 echo " Downloading Script... " >> $log 2>&1
 wget -O $PREFIX/bin/mct https://raw.githubusercontent.com/iqbalfaf/MCT/master/mct.sh
 sleep 1
