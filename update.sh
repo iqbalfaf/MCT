203ddb3cf7bf1d04395f5a4e3a76109f849f103f
Moch Iqbal <iqbalfaf127@gmail.com>
2018-10-20 01:59:23 -0400

    Update Script MCT V1.2

--- update.sh
+++ update.sh
@@ -1,30 +1,32 @@
 #!/usr/bin/env bash
-log=$HOME/mct-update.log
-apt install inetutils
-echo " Updating Script... " >> $log 2>&1
+echo " Updating Script MCT V1.2... " > /dev/null 2>&1
 sleep 1
-echo " Update Repo... " >> $log 2>$1
+echo ""
+echo " Update Repo... " > /dev/null 2>&1
 apt update
 sleep 1
-echo " Update Selesai " >> $log 2>$1
+echo " Update Selesai " > /dev/null 2>&1
 sleep 1
-echo " Installing Curl " >> $log 2>$1
+echo " Installing Curl " > /dev/null 2>&1
 apt install curl
 sleep 1
-echo " Curl Installed " >> $log 2>$1
+echo " Curl Installed " > /dev/null 2>&1
+echo ""
 sleep 1
-echo " Downloading Script... " >> $log 2>&1
-curl https://raw.githubusercontent.com/iqbalfaf/MCT/master/mct.sh >> mct
+echo " Downloading Script... " > /dev/null 2>&1
+curl https://raw.githubusercontent.com/iqbalfaf/MCT/master/mct.sh >> mct  > /dev/null 2>&1
 sleep 1
-echo " Script Downloaded " $log 2>&1
+echo " Script Downloaded " > /dev/null 2>&1
 sleep 1
-echo " Setting Up Script... " $log 2>&1
+echo ""
+echo " Setting Up Script... " > /dev/null 2>&1
 rm $PREFIX/bin/mct
 mv mct $PREFIX/bin
 chmod +x $PREFIX/bin/mct
 sleep 1
-echo " Script Setting Up" $log 2>&1
+echo ""
+echo " Script Setting Up" > /dev/null 2>&1
 sleep 1
-echo " Updating Script Berhasil " $log 2>&1
-echo " Terimakasih " $log 2>&1
-echo " IqbalFAF " $log 2>&1
+echo " Updating Script Berhasil " > /dev/null 2>&1
+echo " Terimakasih " > /dev/null 2>&1
+echo " IqbalFAF " > /dev/null 2>&1
