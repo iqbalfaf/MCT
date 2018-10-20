c746c41e119412d4a45618d1d643125323e6f3b2
Moch Iqbal <iqbalfaf127@gmail.com>
2018-10-20 02:05:31 -0400

    Update MCT V1.2

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
+clear
 echo " Updating Script MCT V1.2... " 
 echo ""
 sleep 1
-echo ""
 echo " Update Repo... "
 apt update > /dev/null 2>&1
 sleep 1
@@ -12,14 +12,12 @@ echo " Installing Curl "
 apt install curl > /dev/null 2>&1
 sleep 1
 echo " Curl Installed " 
-echo ""
 sleep 1
 echo " Downloading Script... " 
 curl https://raw.githubusercontent.com/iqbalfaf/MCT/master/mct.sh >> mct  > /dev/null 2>&1
 sleep 1
 echo " Script Downloaded " 
 sleep 1
-echo ""
 echo " Setting Up Script... " 
 rm $PREFIX/bin/mct
 mv mct $PREFIX/bin
