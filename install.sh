eb233126002f1e269bfc97894290ba5e8579b90c
Iqbal <iqbalfaf127@gmail.com>
2019-02-18 04:32:02 -0800

    Installer

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ yellow='\e[1;33m'
 BlueF='\e[1;34m'
 
 version=1.3
-
+source "$(pwd)/spinner.sh"
 
 echo -e okegreen"                                   ";
 echo " _|      _|    _|_|_|  _|_|_|_|_|  ";
@@ -23,19 +23,28 @@ echo "                               V$version    ";
 echo "     Media Converter Termux        ";
 echo "          IqbalFAF "
 
-chmod +x slib.sh
-. ./slib.sh
+chmod +x spinner.sh
 
-run_ok "apt update" "Updating Repo"
-run_ok "apt install ncurses-utils -y" "Install Ncurses"
-run_ok "apt install ffmpeg -y" "Install ffmpeg"
-run_ok "apt install git -y" "Install Git"
-run_ok "git clone https://github.com/iqbalfaf/MCT.git" "Cloning Script MCT"
+start_spinner 'Updating Repo'
+apt update > /dev/null 2>&1
+stop_spinner $?
+start_spinner 'Install Ncurses'
+apt install ncurses-utils -y > /dev/null 2>&1
+stop_spinner $? 
+start_spinner 'Install ffmpeg'
+apt install ffmpeg -y > /dev/null 2>&1
+stop_spinner $?
+start_spinner 'Install Git'
+apt install git -y > /dev/null 2>&1
+stop_spinner $?
+start_spinner 'Cloning Script MCT'
+git clone https://github.com/iqbalfaf/MCT.git > /dev/null 2>&1
+stop_spinner $?
 cd
 cd MCT/
 mv mct.sh mct
-mv mct $dirbin/
-chmod +x $dirbin/mct
+mv mct $PREFIX/bin/
+chmod +x $PREFIX/bin/mct
 sleep 1
 clear
 echo "Install Script MCT Selesai"
